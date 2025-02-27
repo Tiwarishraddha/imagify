108ee5249cee9c60dadc6aa44d6ae9c3e40b3985
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2025-02-27 22:53:35 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=" bg-red-300 font-bold text-4xl text-center">hello</h1>
+    </div>
+  ) 
+}
+
+export default App
