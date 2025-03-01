8d362204ccc8abc65b07f317d0994016d1a7219b
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2025-03-01 08:45:30 +0530

    Navbar and context page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
-      <h1 className=" bg-red-300 font-bold text-4xl text-center">hello</h1>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen
+     bg-gradient-to-b from-teal-50 to-orange-50'>
+      
+      <Navbar/>
+      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/result' element={<Result/>} />
+        <Route path='/buy' element={<BuyCredit/>} />
+      </Routes>
     </div>
   ) 
 }
