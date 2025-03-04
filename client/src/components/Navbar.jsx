7366d88c73b35aa3eeff7bdab642371f44d313c4
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2025-03-04 17:45:42 +0530

    Login Component added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 const Navbar = () => {
 
-    const {user} = useContext(AppContext)
+    const {user, setShowLogin} = useContext(AppContext)
     
     const navigate = useNavigate()
     
@@ -57,7 +57,8 @@ const Navbar = () => {
               Pricing
             </p>
 
-            <button className='bg-zinc-800 text-white px-7 py-2 
+            <button onClick={() => setShowLogin(true)}
+            className='bg-zinc-800 text-white px-7 py-2 
             sm:px-10 text-sm rounded-full'>
               Login
             </button>
